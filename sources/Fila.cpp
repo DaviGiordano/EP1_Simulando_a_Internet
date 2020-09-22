81c2cd8d65ca80a6409995bdfa7f3b7200a1cd3e
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2020-09-22 18:52:26 -0300

    Adjust method enqueue in Fila

--- sources/Fila.cpp
+++ sources/Fila.cpp
@@ -13,22 +13,21 @@ Fila::~Fila()
 }
 
 bool Fila::enqueue(Datagrama *d) {
-        // condicao para fila nao cheia 
+        // condicao para fila cheia 
     if ( (this->inicio == 0 && this->fim == this->tamanho + 1 ) || this->fim + 1 == this->inicio) {
-        // adiciona datagrama no fim da fila
+        // caso cheia, retorna falso
+        return false;
+    } else {
+        // caso contrario, adiciona datagrama no fim da fila
         this->vetorDeDatagramas[this->fim] = d;
 
-        // atualiza o valor de fim
+        // e atualiza o valor de fim
         if (this->fim == tamanho + 1) {
             this->fim = 0;
         } else  {
             this->fim += 1;
         }
         return true;
-    } else {
-        // caso contrario, retorna falso
-        return false;
     }
 }
-
 
