449908e01d3e9fc61b308a029ec7b2570f704bf7
Davi <davigiordano2704@gmail.com>
2020-09-24 13:17:20 -0300

    Implement Dequeue and imprimir de Fila

--- sources/Fila.cpp
+++ sources/Fila.cpp
@@ -1,11 +1,13 @@
 #include "Fila.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-
-// Inicializa a fila e constroi um vetor de datagramas com espaço tamanho 
+// Inicializa a fila e constroi um vetor de datagramas com espaço tamanho
 Fila::Fila(int tamanho) :
 tamanho (tamanho)
 {
-    this->vetorDeDatagramas = new Datagrama *[tamanho + 1];
+    this->vetorDeDatagramas = new Datagrama *[this->tamanho + 1];
 }
 
 Fila::~Fila()
@@ -13,7 +15,7 @@ Fila::~Fila()
 }
 
 bool Fila::enqueue(Datagrama *d) {
-        // condicao para fila cheia 
+        // condicao para fila cheia
     if ( (this->inicio == 0 && this->fim == this->tamanho + 1 ) || this->fim + 1 == this->inicio) {
         // caso cheia, retorna falso
         return false;
@@ -22,7 +24,7 @@ bool Fila::enqueue(Datagrama *d) {
         this->vetorDeDatagramas[this->fim] = d;
 
         // e atualiza o valor de fim
-        if (this->fim == tamanho + 1) {
+        if (this->fim == this->tamanho + 1) {
             this->fim = 0;
         } else  {
             this->fim += 1;
@@ -33,4 +35,51 @@ bool Fila::enqueue(Datagrama *d) {
 
 bool Fila::isEmpty() {
     return this->inicio == this->fim;
+}
+
+Datagrama* Fila::dequeue(){
+    if(this->isEmpty()){
+        return NULL;
+    } else {
+
+        Datagrama* aux = this->vetorDeDatagramas[this->inicio];
+        if(this->inicio == this->tamanho + 1){
+            this->inicio = 0;
+        } else {
+            this->inicio += 1;
+        }
+        return aux;
+    }
+}
+//!! Ainda tenho que fazer "dar a volta" quando necessário
+void Fila::imprimir(){
+    cout << "tamanho - " << this->tamanho << endl << endl;
+    cout << "inicio - " << this->inicio << endl << endl;
+    cout << "fim - " << this->fim << endl << endl;
+    cout <<  "Fila, na ordem:" << endl << endl;
+
+    for(int i = this->inicio; i != (this->fim - 1);){
+
+        cout <<"Posicao no vetor: " << i << endl;
+        vetorDeDatagramas[i]->imprimir();
+
+        if(i == this->tamanho){
+            i = 0;
+        }
+        else{
+            i++;
+        }
+    }
+
+    /*
+    int i = this->inicio;
+    while(i != this->fim){
+        if(i == this->tamanho){
+            i = 0;
+        }else{ i++; }
+        cout <<"Posição no vetor: " << i << endl;
+        vetorDeDatagramas[i]->imprimir();
+    }
+    cout<< endl;
+    */
 }
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Datagrama.h"
+#include "Fila.h"
 using namespace std;
 
 //no vídeo sobre construtores e destrutures ele adicionou esses parâmetros. Você sabe o que são?
@@ -9,12 +10,23 @@ main(int argc, char **argv){
 
     //TESTANDO MÉTODO bool Datagrama::ativo
 
-    Datagrama *pacoteAtivo = new Datagrama(1,2,78,"oi");
-    Datagrama *pacoteInativo = new Datagrama(3,4,0,"tchau");
-    cout << "Pacote Ativo: " << endl;
-    pacoteAtivo->imprimir();
-    cout << "Pacote Inativo: " <<endl;
-    pacoteInativo->imprimir();
+    Datagrama *pacotePrimeiro = new Datagrama(1,2,78,"oi");
+    Datagrama *pacoteSegundo = new Datagrama(3,4,0,"tchau");
+    Datagrama *pacoteTerceiro = new Datagrama(2,7,5,"Feliz");
+    Datagrama *pacoteQuarto = new Datagrama (6,7,1,"Natal");
+    //cout << "Pacote Ativo: " << endl;
+    //pacoteAtivo->imprimir();
+    //cout << "Pacote Inativo: " <<endl;
+   // pacoteInativo->imprimir();
 
+    Fila *filaTeste = new Fila(3);
+
+    cout << "teste 1" << endl;
+    filaTeste->enqueue(pacotePrimeiro);
+    filaTeste->enqueue(pacoteSegundo);
+    filaTeste->enqueue(pacoteTerceiro);
+ //PRECISAMOS testarrr maiss!!
+    filaTeste->enqueue(pacoteQuarto);
+    filaTeste->imprimir();
 
 }
