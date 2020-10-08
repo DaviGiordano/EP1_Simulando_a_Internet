c9955df100ff4c3c40d16099430d929ce281f16e
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2020-10-08 19:15:09 -0300

    Clean the main code

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Datagrama.h"
-#include "Fila.h"
+#include "Fila.h" 
 #include "Roteador.h"
 #include "TabelaDeRepasse.h"
 #include "Rede.h"
@@ -10,9 +10,12 @@ using namespace std;
 
 #define NUMERO_DE_ROTEADORES_NA_REDE 6
 //Devolve um roteador com o endereco especificado
-Roteador* criarRoteador(int endereco);
-void definirPadrao(Roteador* rBase, Roteador* rPadrao);
-void definirAdjacente(Roteador* rBase, int endereco, Roteador* radjacente);
+// Roteador* criarRoteador(int endereco);
+Roteador** criarRoteadores(int vetorDeEnderecos[], int quantidadeDeRoteadores);
+// void definirPadrao(Roteador* rBase, Roteador* rPadrao);
+void definirPadroes(Roteador *matrizDeRotBaseERotOrigem[][2], int i);
+void definirAdjacentes(Roteador *matrizDeRotBaseERotAdjacente[][2], int i, int vetorEnderecos[]);
+// void definirAdjacente(Roteador* rBase, int endereco, Roteador* radjacente);
 
 void telaDeEnviarDatagrama(Rede *rede);
 void enviarDatagrama(int enderecoOrigem, int enderecoDestino, int ttl, string dado, Rede *rede);
@@ -23,43 +26,72 @@ int telaInicial();
 main(int argc, char **argv){
 
     //criar os roteadores
-    Roteador* r1 = criarRoteador(1);
-    Roteador* r2 = criarRoteador(2);
-    Roteador* r3 = criarRoteador(3);
-    Roteador* r4 = criarRoteador(4);
-    Roteador* r5 = criarRoteador(5);
-    Roteador* r6 = criarRoteador(6);
+    int enderecos[] = {1, 2, 3, 4, 5, 6};
+    
+    // Roteador* r1 = criarRoteador(1);
+    // Roteador* r2 = criarRoteador(2);
+    // Roteador* r3 = criarRoteador(3);
+    // Roteador* r4 = criarRoteador(4);
+    // Roteador* r5 = criarRoteador(5);
+    // Roteador* r6 = criarRoteador(6);
+    Roteador** vetorDeRoteadores = criarRoteadores(enderecos, 6);
 
-    //definir os roteadores padrão
-    definirPadrao(r1,r2);
-    definirPadrao(r2,r5);
-    definirPadrao(r3,r2);
-    definirPadrao(r4,r5);
-    definirPadrao(r5,r2);
-    definirPadrao(r6,r5);
+    //definir os roteadores padrï¿½o
+    // definirPadrao(r1,r2);
+    // definirPadrao(r2,r5);
+    // definirPadrao(r3,r2);
+    // definirPadrao(r4,r5);
+    // definirPadrao(r5,r2);
+    // definirPadrao(r6,r5);
+
+    Roteador *matrizDeMapeamentoDePadroes[6][2] = {
+        vetorDeRoteadores[0], vetorDeRoteadores[1],
+        vetorDeRoteadores[1], vetorDeRoteadores[4], 
+        vetorDeRoteadores[2], vetorDeRoteadores[1], 
+        vetorDeRoteadores[3], vetorDeRoteadores[4], 
+        vetorDeRoteadores[4], vetorDeRoteadores[1], 
+        vetorDeRoteadores[5], vetorDeRoteadores[4] 
+    };
+
+    definirPadroes(matrizDeMapeamentoDePadroes, 6);
 
     //definir os roteadores Ajacentes
-    definirAdjacente(r1,4,r4);
-    definirAdjacente(r2,1,r1);
-    definirAdjacente(r2,3,r3);
-    definirAdjacente(r3,6,r6);
-    definirAdjacente(r4,1,r1);
-    definirAdjacente(r5,4,r4);
-    definirAdjacente(r5,6,r6);
-    definirAdjacente(r6,3,r3);
+
+    Roteador *matrizDeMapeamentoDeAdjacentes[8][2] = {
+        vetorDeRoteadores[0], vetorDeRoteadores[3],
+        vetorDeRoteadores[1], vetorDeRoteadores[0], 
+        vetorDeRoteadores[1], vetorDeRoteadores[2], 
+        vetorDeRoteadores[2], vetorDeRoteadores[5], 
+        vetorDeRoteadores[3], vetorDeRoteadores[0], 
+        vetorDeRoteadores[4], vetorDeRoteadores[3],
+        vetorDeRoteadores[4], vetorDeRoteadores[5],
+        vetorDeRoteadores[5], vetorDeRoteadores[2] 
+    };
+
+    int vetorDeEnderecosAdjacentes[] = {4, 1, 3, 6, 1, 4, 6, 3};
+
+    definirAdjacentes(matrizDeMapeamentoDeAdjacentes, 8, vetorDeEnderecosAdjacentes);
+
+
+    // definirAdjacente(r1,4,r4);
+    // definirAdjacente(r2,1,r1);
+    // definirAdjacente(r2,3,r3);
+    // definirAdjacente(r3,6,r6);
+    // definirAdjacente(r4,1,r1);
+    // definirAdjacente(r5,4,r4);
+    // definirAdjacente(r5,6,r6);
+    // definirAdjacente(r6,3,r3);
 
     //criar vetor de roteadores
-    Roteador** vetorDeRoteadores = new Roteador*[6];
 
     //preencher vetor de roteadores
-    vetorDeRoteadores[0] = r1;
-    vetorDeRoteadores[1] = r2;
-    vetorDeRoteadores[2] = r3;
-    vetorDeRoteadores[3] = r4;
-    vetorDeRoteadores[4] = r5;
-    vetorDeRoteadores[5] = r6;
+    // vetorDeRoteadores[0] = r1;
+    // vetorDeRoteadores[1] = r2;
+    // vetorDeRoteadores[2] = r3;
+    // vetorDeRoteadores[3] = r4;
+    // vetorDeRoteadores[4] = r5;
+    // vetorDeRoteadores[5] = r6;
 
-    //r1->imprimir();
     //criar rede com tais roteadores
     Rede* rede = new Rede(vetorDeRoteadores, 6);
 
@@ -83,16 +115,39 @@ main(int argc, char **argv){
 
 }
 
-Roteador* criarRoteador(int endereco){
-    return new Roteador(endereco);
-}
-void definirPadrao(Roteador* rBase, Roteador* rPadrao){
-    rBase->getTabela()->setPadrao(rPadrao);
+// Roteador* criarRoteador(int endereco){
+//     return new Roteador(endereco);
+// }
+
+Roteador** criarRoteadores(int vetorDeEnderecos[], int quantidadeDeRoteadores) {
+    Roteador** vetorDeRoteadores = new Roteador*[quantidadeDeRoteadores];
+    for (int i = 0; i < quantidadeDeRoteadores; i++) {
+        vetorDeRoteadores[i] = new Roteador(vetorDeEnderecos[i]);
+    }
+
+    return vetorDeRoteadores;
 }
 
-void definirAdjacente(Roteador* rBase, int endereco, Roteador* rAdjacente){
-    rBase->getTabela()->mapear(endereco, rAdjacente);
+// void definirPadrao(Roteador* rBase, Roteador* rPadrao){
+//     rBase->getTabela()->setPadrao(rPadrao);
+// }
+
+void definirPadroes(Roteador *matrizDeRotBaseERotOrigem[][2], int i) {
+    for (int linha = 0; linha < i; linha++) {
+        matrizDeRotBaseERotOrigem[linha][0]->getTabela()->setPadrao(matrizDeRotBaseERotOrigem[linha][1]);
+    }
 }
+
+// void definirAdjacente(Roteador* rBase, int endereco, Roteador* rAdjacente){
+//     rBase->getTabela()->mapear(endereco, rAdjacente);
+// }
+
+void definirAdjacentes(Roteador *matrizDeRotBaseERotAdjacente[][2], int i, int vetorEnderecos[]) {
+    for (int linha = 0; linha < i; linha++) {
+        matrizDeRotBaseERotAdjacente[linha][0]->getTabela()->mapear(vetorEnderecos[linha], matrizDeRotBaseERotAdjacente[linha][1]);
+    }
+}
+
 
 int telaInicial() {
     int tela;
